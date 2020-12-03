002a2f1b0b00efe457ff393977428ab9f41e72c9
johnFarkasolo <farkasolo@mail.ru>
2020-12-03 14:59:11 +0100

    Complited right paper visualisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,85 @@
 import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: theme.spacing(10),
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+    currencyInput: {
+      minWidth: 'calc(70% - 10px)',
+      marginRight: 10,
+    },
+    currencyType: {
+      minWidth: '30%',
+      textAlign: 'left',
+    },
+    cryptoInputBox: {
+      marginTop: '20px',
+      marginBottom: '20px',
+    },
+  }),
+);
 
 function App() {
-  return <div className="App"></div>;
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="lg" className={classes.root}>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+            <div className={classes.cryptoInputBox}>
+              <FormControl className={classes.currencyInput}>
+                <TextField fullWidth label="total" />
+              </FormControl>
+              <FormControl className={classes.currencyType}>
+                <InputLabel id="currencySelect">Сurrency</InputLabel>
+                <Select id="currencySelect" value={10}>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <div className={classes.cryptoInputBox}>
+              <FormControl className={classes.currencyInput}>
+                <TextField fullWidth label="total" />
+              </FormControl>
+              <FormControl className={classes.currencyType}>
+                <InputLabel id="currencySelect">Сurrency</InputLabel>
+                <Select id="currencySelect" value={10}>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <Typography variant="h5" component="h5">
+              0.271598 USD
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default App;
